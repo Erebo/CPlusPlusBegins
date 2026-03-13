c23d9ef03d4aaafe323091c0da422897a277a4e2
erebo <rahmanmahadi177153@gmail.com>
2026-03-14 02:50:15 +0600

    i/o basic

---
+++ Basics/general.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main(){
+string name;
+int age;
+cout<<"Enter your name: ";
+getline(cin,name);
+cout<<"Enter your age: ";
+cin>>age;
+cout<<"Your name is "<<name<<"\n";
+cout<<"Your age is "<<age<<"\n";
+
+    return 0;
+}
